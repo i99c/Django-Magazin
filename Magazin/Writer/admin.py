e16d0e0445708ebe8bb1a5033497d50894fb6114
Ipek Canli <119710326+i99c@users.noreply.github.com>
2024-07-10 00:59:41 +0300

    category eklendi

--- Magazin/Writer/admin.py
+++ Magazin/Writer/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Category
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at', 'updated_at')
     search_fields = ('title', 'content')
     list_filter = ('created_at', 'updated_at')
+
+
+    admin.site.register(Category)
--- Magazin/Writer/models.py
+++ Magazin/Writer/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 from ckeditor.fields import RichTextField
 
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)  # Null değerine izin verin
     content = RichTextField()  # Using RichTextField for the content
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
